49bbdd9f65245c4053665dc5c583acbdd8e1bc7c
我是一只大笨熊 <595717967@qq.com>
2019-01-07 22:36:42 +0800

    毕设作品Raiser

---
+++ RiserGame/Assets/scripts/Boss.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public GameObject RealBoss;
+    public BossFeather[] Features;
+
+    private bool m_Running;
+    private int m_Index;
+    private float m_Duration;
+    private float m_MaxDuration;
+    private float m_FeatureDuration;
+    private int m_Times;
+    private int m_MaxTimes;
+    private float m_Sign;
+    private List<Vector3> m_Points;
+    private float m_Speed;
+    private System.Action m_Callback;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Running = false;
+
+        foreach (var feature in Features)
+        {
+            feature.gameObject.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!m_Running)
+        {
+            return;
+        }
+
+        if (m_Index == 1 || m_Index == 2)
+        {
+            var target = m_Points[0];
+            transform.position = Vector3.MoveTowards(transform.position, target, m_Speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, target) <= 0.00001f)
+            {
+                m_Points.RemoveAt(0);
+                if (m_Points.Count == 0)
+                {
+                    m_Running = false;
+                    if (m_Callback != null)
+                    {
+                        m_Callback.Invoke();
+                    }
+                }
+                else
+                {
+                    gameObject.transform.LookAt(m_Points[0]);
+                }
+            }
+        }
+        else
+        {
+            m_Duration += Time.deltaTime * m_Sign;
+            if (m_Duration <= 0)
+            {
+                m_Sign = 1;
+                m_Duration = 0;
+
+                if (++m_Times >= m_MaxTimes)
+                {
+                    m_Running = false;
+                    if (m_Callback != null)
+                    {
+                        m_Callback.Invoke();
+                    }
+                }
+            }
+            else if (m_Duration >= m_MaxDuration)
+            {
+                m_Sign = -1;
+                m_Duration = m_MaxDuration;
+            }
+            float t = m_Duration / m_MaxDuration;
+            transform.position = (1 - t) * (1 - t) * m_Points[0] + 2 * t * (1 - t) * m_Points[1] + t * t * m_Points[2];
+            transform.right = Vector3.Normalize((m_Sign > 0 ? m_Points[0] : m_Points[2]) - transform.position);
+
+            //发射羽毛
+            m_FeatureDuration += Time.deltaTime;
+            if (m_FeatureDuration >= 2)
+            {
+                m_FeatureDuration = 0;
+                Features[0].transform.parent.position = transform.position;
+                foreach (var feature in Features)
+                {
+                    feature.Shoot();
+                }
+            }
+        }
+    }
+
+    public void MoveLoop(int index, Vector3 point1, Vector3 point2, int times, float duration, System.Action callback)
+    {
+        m_Running = true;
+        m_Index = index;
+        m_Points = new List<Vector3>();
+        for (int i = 0; i < times; ++i)
+        {
+            m_Points.Add(point1);
+            m_Points.Add(point2);
+        }
+        m_Callback = callback;
+
+        gameObject.transform.position = m_Points[0];
+        gameObject.transform.LookAt(m_Points[1]);
+        m_Points.RemoveAt(0);
+
+        m_Speed = Vector3.Distance(point1, point2) * 2 / (duration / times);
+
+        switch (index)
+        {
+            case 1:
+                RealBoss.transform.localEulerAngles = new Vector3(90, 0, 0);
+                break;
+            case 2:
+                RealBoss.transform.localEulerAngles = new Vector3(0, -90, -90);
+                break;
+        }
+    }
+
+    public void MoveBezier(Vector3 point1, Vector3 point2, Vector3 point3, int times, float duration, System.Action callback)
+    {
+        m_Running = true;
+        m_Index = 3;
+        m_Duration = 0;
+        m_MaxDuration = duration / (times * 2);
+        m_Times = 0;
+        m_MaxTimes = times;
+        m_Sign = 1;
+
+        m_Points = new List<Vector3>
+        {
+            point1,
+            point2,
+            point3
+        };
+
+        RealBoss.transform.localEulerAngles = new Vector3(0, 0, 90);
+    }
+}
---
+++ RiserGame/Assets/scripts/BossAI.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : MonoBehaviour
+{
+    public Boss Boss1;
+    public Boss Boss2;
+
+    public UnityEngine.UI.Image LifeImage;
+    private int m_Life;
+
+    public float Duration1;
+    public Transform Boss1DiveStartPoint;
+    public Transform Boss1DiveEndPoint;
+    public Transform Boss2DiveStartPoint;
+    public Transform Boss2DiveEndPoint;
+
+    public float Duration2;
+    public Transform Boss1CollideStartPoint;
+    public Transform Boss1CollideEndPoint;
+    public Transform Boss2CollideStartPoint;
+    public Transform Boss2CollideEndPoint;
+
+    public float Duration3;
+    public Transform Boss1BezierPoint1;
+    public Transform Boss1BezierPoint2;
+    public Transform Boss1BezierPoint3;
+    public Transform Boss2BezierPoint1;
+    public Transform Boss2BezierPoint2;
+    public Transform Boss2BezierPoint3;
+
+    private int m_CurrentIndex;
+    private float m_CurrentDuration;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Life = 10;
+        m_CurrentIndex = 0;
+        LifeImage.transform.parent.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_CurrentDuration += Time.deltaTime;
+        switch (m_CurrentIndex)
+        {
+            case 0:     //检测玩家是否进入boss区域
+                break;
+            case 1:     //第一阶段
+                break;
+        }
+    }
+
+    //碰撞检测
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {//开始第一阶段
+            EnterFirstStage();
+        }
+    }
+
+    void EnterFirstStage()
+    {
+       
+        StartCoroutine(DelayToInvoke.DelayToInvokeDo(() =>
+        {
+            m_CurrentIndex = 1;
+            Boss1.MoveLoop(m_CurrentIndex, Boss1DiveStartPoint.position, Boss1DiveEndPoint.position, 10, Duration1, EnterSecondStage);
+            Boss2.MoveLoop(m_CurrentIndex, Boss2DiveStartPoint.position, Boss2DiveEndPoint.position, 10, Duration1, null);
+        }, 3f));
+       
+
+        LifeImage.transform.parent.gameObject.SetActive(true);
+    }
+
+    void EnterSecondStage()
+    {
+        m_CurrentIndex = 2;
+        Boss1.MoveLoop(m_CurrentIndex, Boss1CollideStartPoint.position, Boss1CollideEndPoint.position, 10, Duration2, EnterThirdStage);
+        Boss2.MoveLoop(m_CurrentIndex, Boss2CollideStartPoint.position, Boss2CollideEndPoint.position, 10, Duration2, null);
+    }
+
+    void EnterThirdStage()
+    {
+        m_CurrentIndex = 3;
+        Boss1.MoveBezier(Boss1BezierPoint1.position, Boss1BezierPoint2.position, Boss1BezierPoint3.position, 5, Duration3, null);
+        Boss2.MoveBezier(Boss2BezierPoint1.position, Boss2BezierPoint2.position, Boss2BezierPoint3.position, 5, Duration3, null);
+    }
+
+    public void LifeChange()
+    {
+        --m_Life;
+        LifeImage.fillAmount = m_Life / 10.0f;
+        if (m_Life == 0)
+        {//boss死亡
+
+        }
+    }
+}
